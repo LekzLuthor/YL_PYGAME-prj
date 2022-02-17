08e8a0c7340910e902df50d8ce37298a742038b3
LekzLuthor <koshanskiy00@mail.ru>
2022-02-17 23:14:40 +0500

    collisions

--- resourse/tile.py
+++ resourse/tile.py
@@ -5,5 +5,8 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('../textures/background/rock_wall.png').convert_alpha()
+        self.image = pygame.image.load('../textures/background/second_rock+shadow.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
+
+        # hitboxes
+        self.hitbox = self.rect.inflate(0, -10)

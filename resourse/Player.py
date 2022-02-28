c2ddf5af3a8af2d83611c89c4b22816ede9523ee
LekzLuthor <koshanskiy00@mail.ru>
2022-03-01 01:43:19 +0500

    enemy + damage map rework

--- resourse/Player.py
+++ resourse/Player.py
@@ -1,10 +1,15 @@
 import pygame
 from settings import *
 from support import import_folder
+from entity import Entity
 
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups, obstacle_sprites, create_attack, destroy_attack):
+class Player(Entity):
+
+    def __init__(
+            self, pos, groups, obstacle_sprites, create_attack, destroy_attack,
+            create_magic
+    ):
         super().__init__(groups)
         self.image = pygame.image.load('../textures/Player/positions/player_main.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
@@ -13,10 +18,6 @@ class Player(pygame.sprite.Sprite):
         # graphic setup
         self.import_player_assets()
         self.status = 'down'
-        self.frame_index = 0
-        self.animation_speed = 0.15
-
-        self.direction = pygame.math.Vector2()
         self.speed = 5
 
         self.attacking = False
@@ -34,14 +35,26 @@ class Player(pygame.sprite.Sprite):
         self.weapon_switch_time = None
         self.switch_duration_cooldown = 200
 
+        # magic
+        self.create_magic = create_magic
+        self.magic_index = 0
+        self.magic = list(magic_data.keys())[self.magic_index]
+        self.can_switch_magic = True
+        self.magic_switch_time = None
+
         # stats
         self.stats = {'health': 100, 'energy': 60, 'attack': 10, 'magic': 4, 'speed': 5}
         self.max_stats = {'health': 300, 'energy': 140, 'attack': 20, 'magic': 10, 'speed': 10}
         self.upgrade_cost = {'health': 100, 'energy': 100, 'attack': 100, 'magic': 100, 'speed': 100}
-        self.health = self.stats['health'] * 0.8
-        self.energy = self.stats['energy'] * 0.3
+        self.health = self.stats['health']
+        self.energy = self.stats['energy']
         self.exp = 123
         self.speed = self.stats['speed']
+
+        # damage timer
+        self.vulnerable = True
+        self.hurt_time = None
+        self.invulnerability_duration = 500
 
     def import_player_assets(self):
         character_path = '../textures/Player/'
@@ -112,10 +125,15 @@ class Player(pygame.sprite.Sprite):
                 self.create_attack()
 
             # magic input
-            if keys[pygame.K_LCTRL]:
+            if keys[pygame.K_LSHIFT]:
                 self.attacking = True
                 self.attack_time = pygame.time.get_ticks()
+                style = list(magic_data.keys())[self.magic_index]
+                strength = list(magic_data.values())[self.magic_index]['strength'] + self.stats['magic']
+                cost = list(magic_data.values())[self.magic_index]['cost']
+                self.create_magic(style, strength, cost)
 
+            # weapon switch
             if keys[pygame.K_q] and self.can_switch_weapon:
                 self.can_switch_weapon = False
                 self.weapon_switch_time = pygame.time.get_ticks()
@@ -125,6 +143,17 @@ class Player(pygame.sprite.Sprite):
                 else:
                     self.weapon_index = 0
                 self.weapon = list(weapon_data.keys())[self.weapon_index]
+
+            # magic switch
+            if keys[pygame.K_e] and self.can_switch_magic:
+                self.can_switch_magic = False
+                self.magic_switch_time = pygame.time.get_ticks()
+
+                if self.magic_index < len(list(magic_data.keys())) - 1:
+                    self.magic_index += 1
+                else:
+                    self.magic_index = 0
+                self.magic = list(magic_data.keys())[self.magic_index]
 
             # 12345 switch weapon buttons
             if keys[pygame.K_1]:
@@ -143,21 +172,11 @@ class Player(pygame.sprite.Sprite):
                 self.weapon_index = 4
                 self.weapon = list(weapon_data.keys())[self.weapon_index]
 
-    def move(self, speed):
-        if self.direction.magnitude() != 0:
-            self.direction = self.direction.normalize()
-
-        self.hitbox.x += self.direction.x * speed
-        self.collision('horizontal')
-        self.hitbox.y += self.direction.y * speed
-        self.collision('vertical')
-        self.rect.center = self.hitbox.center
-
     def cooldowns(self):
         current_time = pygame.time.get_ticks()
 
         if self.attacking:
-            if current_time - self.attack_time >= self.attack_cooldown:
+            if current_time - self.attack_time >= self.attack_cooldown + weapon_data[self.weapon]['cooldown']:
                 self.attacking = False
                 self.destroy_attack()
 
@@ -165,22 +184,13 @@ class Player(pygame.sprite.Sprite):
             if current_time - self.weapon_switch_time >= self.switch_duration_cooldown:
                 self.can_switch_weapon = True
 
-    def collision(self, direction):
-        if direction == 'horizontal':
-            for sprite in self.obstacle_sprites:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.x > 0:
-                        self.hitbox.right = sprite.hitbox.left
-                    if self.direction.x < 0:
-                        self.hitbox.left = sprite.hitbox.right
+        if not self.can_switch_magic:
+            if current_time - self.magic_switch_time >= self.switch_duration_cooldown:
+                self.can_switch_magic = True
 
-        if direction == 'vertical':
-            for sprite in self.obstacle_sprites:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.y > 0:
-                        self.hitbox.bottom = sprite.hitbox.top
-                    if self.direction.y < 0:
-                        self.hitbox.top = sprite.hitbox.bottom
+        if not self.vulnerable:
+            if current_time - self.hurt_time >= self.invulnerability_duration:
+                self.vulnerable = True
 
     def animate(self):
         animation = self.animations[self.status]
@@ -193,6 +203,17 @@ class Player(pygame.sprite.Sprite):
         # setting image
         self.image = animation[int(self.frame_index)]
         self.rect = self.image.get_rect(center=self.hitbox.center)
+
+        if not self.vulnerable:
+            alpha = self.wave_value()
+            self.image.set_alpha(alpha)
+        else:
+            self.image.set_alpha(255)
+
+    def get_full_weapon_damage(self):
+        base_damage = self.stats['attack']
+        weapon_damage = weapon_data[self.weapon]['damage']
+        return base_damage + weapon_damage
 
     def update(self):
         self.input()
--- resourse/level.py
+++ resourse/level.py
@@ -1,11 +1,11 @@
-import pygame
-from settings import *
-from tile import Tile
 from Player import Player
+from enemy import Enemy
+from settings import *
 from support import *
-from debug_mode import debug
-from weapon import Weapon
+from tile import Tile
 from ui import UI
+from weapon import Weapon
+from random import choice
 
 
 class Level:
@@ -18,6 +18,8 @@ class Level:
 
         # attack sprites
         self.current_attack = None
+        self.attack_sprites = pygame.sprite.Group()
+        self.attackable_sprites = pygame.sprite.Group()
 
         self.create_map()
 
@@ -27,11 +29,12 @@ class Level:
     def create_map(self):
         layouts = {
             'boundary': import_csv_layout('../map/main_game._block.csv'),
-            'grass': import_csv_layout('../map/main_game._invisible_decoration.csv'),
-            'object': import_csv_layout('../map/main_game._decoration.csv')
+            'grass': import_csv_layout('../map/main_game._place_holders_ff.csv'),
+            'object': import_csv_layout('../map/main_game._decoration.csv'),
+            'entities': import_csv_layout('../map/main_game._Entitles.csv')
         }
         graphics = {
-            # 'grass': import_folder('../textures/Grass'),
+            'grass': import_folder('../textures/grass'),
             'objects': import_folder('../textures/Objects'),
             'special_objects': import_folder('../textures/special')
         }
@@ -46,7 +49,9 @@ class Level:
                             Tile((x, y), [self.obstacle_sprites],
                                  'invisible')
                         if style == 'grass':
-                            pass
+                            random_grass_image = choice(graphics['grass'])
+                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites, self.attackable_sprites],
+                                 'grass', random_grass_image)
                         if style == 'object':
                             surf = graphics['objects'][int(col)]
                             Tile((x, y), [self.visible_sprites, self.obstacle_sprites],
@@ -55,21 +60,64 @@ class Level:
                             surf = graphics['special_objects'][int(col)]
                             Tile((x, y), [self.visible_sprites],
                                  'object', surf)
+                        if style == 'entities':
+                            if col == '0':
+                                self.player = Player(
+                                    (x + 25, y),
+                                    [self.visible_sprites],
+                                    self.obstacle_sprites,
+                                    self.create_attack,
+                                    self.destroy_attack,
+                                    self.create_magic
+                                )
+                            else:
+                                if col == '1':
+                                    monster_name = 'bamboo'
+                                elif col == '2':
+                                    monster_name = 'raccoon'
+                                elif col == '3':
+                                    monster_name = 'spirit'
+                                elif col == '4':
+                                    monster_name = 'squid'
 
-        self.player = Player(
-            (1950, 450), [self.visible_sprites], self.obstacle_sprites, self.create_attack, self.destroy_attack)
+                                Enemy(monster_name, (x, y),
+                                      [self.attackable_sprites, self.visible_sprites],
+                                      self.obstacle_sprites,
+                                      self.damage_player)
 
     def create_attack(self):
-        self.current_attack = Weapon(self.player, [self.visible_sprites])
+        self.current_attack = Weapon(self.player, [self.visible_sprites, self.attack_sprites])
+
+    def create_magic(self, style, strength, cost):
+        pass
 
     def destroy_attack(self):
         if self.current_attack:
             self.current_attack.kill()
         self.current_attack = None
 
+    def player_attack_logic(self):
+        if self.attack_sprites:
+            for attack_sprite in self.attack_sprites:
+                collision_sprites = pygame.sprite.spritecollide(attack_sprite, self.attackable_sprites, False)
+                if collision_sprites:
+                    for target_sprite in collision_sprites:
+                        if target_sprite.sprite_type == 'grass':
+                            target_sprite.kill()
+                        else:
+                            target_sprite.get_damage(self.player, attack_sprite.sprite_type)
+
+    def damage_player(self, amount, attack_type):
+        if self.player.vulnerable:
+            self.player.health -= amount
+            self.player.vulnerable = False
+            self.player.hurt_time = pygame.time.get_ticks()
+
     def run(self):
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        self.visible_sprites.enemy_update(self.player)
+        self.player_attack_logic()
         self.ui.display(self.player)
 
 
@@ -97,3 +145,9 @@ class YSortCameraGroup(pygame.sprite.Group):
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
+
+    def enemy_update(self, player):
+        enemy_sprites = [sprite for sprite in self.sprites() if
+                         hasattr(sprite, 'sprite_type') and sprite.sprite_type == 'enemy']
+        for enemy in enemy_sprites:
+            enemy.enemy_update(player)
